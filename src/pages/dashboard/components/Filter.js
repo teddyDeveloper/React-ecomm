74f6f5878868f7bce1856782bd38c71e98f02657
TeddyDeveloper <Tedros.Tedla@amtrustgroup.com>
2020-03-05 15:15:31 +0000

    Modified fillter

--- src/pages/dashboard/components/Filter.js
+++ src/pages/dashboard/components/Filter.js
@@ -11,7 +11,7 @@ export default class Filter extends Component {
     super(props)
     this.FILTER_CONFIG = {
       order: ['Ascending', 'Descending'],
-      department: ['Men', 'Women','Kids','Home'],
+      department: ['Men', 'Women','Kids','Home','Electrical','Entertainment'],
       price: ['Less Than £29', '£29 - £39', '£39 - £49', '£49 - £89', 'Greater Than $89']
     }
     this.initialState = {}
